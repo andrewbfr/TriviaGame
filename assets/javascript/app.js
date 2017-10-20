fe11a56eeef048a595aeb34473ce35cede02d0ef
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-10-19 19:36:06 -0600

    periodic and regular commits

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+//oh I don't have anything yet?
+
+//PSEUDO CODING ARHHDS-------------
+// A start button that loads the trivia question object (with answers included in it)
+//
+// questions are created in new divs, etc upon the click of "start" button.
+
+// so, in this way, the hard coding isn't in the HTML file but rather, in JS. 
+// this is a little more interesting..
+
+//DOM Manipulation
+
+//---------------------------------
+//Variables
+
+var correctAnswers;
+var wrongAnswers;
+var unAnswered;
+
+
+
+
+//---------------------------------
+//Functions
+
+
+
+
+
+
+//---------------------------------
+//Main Processes
