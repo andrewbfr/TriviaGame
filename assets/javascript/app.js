29efa97cf7369fa104b0fe060fd9daa2e2031c35
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-10-20 21:19:04 -0600

    lots of pseudocoding

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,21 @@
-//oh I don't have anything yet?
 
 ////////---------------
-//MOUNTAIN THEMED TRIVIA. solidify.
+
+//MOUNTAIN THEMED TRIVIA. solidified content.
+
 /////-----------------
+
+
+
+
+//-------------- *Conditions of Game for Successful Grading* ------
+//*Countdown starts when "start" button is pressed
+//*You can only select one answer at a time
+//*End of timer --OR-- clicking "done"/"finish" will pass answers into answer check/roundup/whatever final tally
+//*End of timer/etc. will also take to new page with the roundup/tally displayed
+//*Have a "restart" or "play again" button to do the questions over again on this tally page
+//*
+
 
 //PSEUDO CODING ARHHDS-------------
 // A start button that loads the trivia question object (with answers included in it)
@@ -30,11 +43,15 @@
 //answer group
 
 //on button clicks, have the correct answers add an index position to a 
-// variable, the incorrect answers will do the same to a different 
-// variable
+// variable, the incorrect answers will do the same to a different variable
+// use "this" to refer to on.click buttons
+// populate variables with .push
 // the onclick event in each "correct" and "incorrect" answer will populate (i++) the relevant
 // variable, the "finish" button (new idea as of this comment) will tally all of these and 
 // .text display them in the div which displays correct answers
+// "start" button will add or clear the hidden/visible attribute given to the question and answer divs
+// "done" button will add or clear the same attribute, such that the questions are no longer visible and the answer tally now is
+// "replay" button will again add or clear the attribute to the tally page and open visibility to the question divs
 //DOM Manipulation
 
 //---------------------------------
