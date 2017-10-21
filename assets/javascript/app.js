024bd03aaeaf1354f92a8022887550546c242a2b
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-10-20 21:01:29 -0600

    Well, pseudocoding and decisions about content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,40 @@
 //oh I don't have anything yet?
 
+////////---------------
+//MOUNTAIN THEMED TRIVIA. solidify.
+/////-----------------
+
 //PSEUDO CODING ARHHDS-------------
 // A start button that loads the trivia question object (with answers included in it)
 // Start button also has to start the timer that delivers some amount of 
 //milliseconds to finish guessing all of the questions
+
 // "hard" or "advanced" game offers new pages for each question and starts a 
 // new time with each page/question load
+
 // questions are created in new divs, etc upon the click of "start" button.
+// new divs for questions may not be necessary, can practice creating multiple
+//
 
 // so, in this way, the hard coding isn't in the HTML file but rather, in JS. 
 // this is a little more interesting..
+
 // need to find a way to keep track of which answers are "correct" and which
 // are "wrong", the data-attribute covered in class might be a useful option
+
 // some class designation, data attribute or something that isn't directly
 // on the visual presentation (even if it's visible in the console).
 
+//an on click event listener, where if button with correct answer 
+//class is pressed then add a tally, and else tally the incorrect 
+//answer group
+
+//on button clicks, have the correct answers add an index position to a 
+// variable, the incorrect answers will do the same to a different 
+// variable
+// the onclick event in each "correct" and "incorrect" answer will populate (i++) the relevant
+// variable, the "finish" button (new idea as of this comment) will tally all of these and 
+// .text display them in the div which displays correct answers
 //DOM Manipulation
 
 //---------------------------------
